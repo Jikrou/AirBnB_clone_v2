10b4e3721380dd6eb02f257079c67214a1f5ae77
Jikrou <121866583+Jikrou@users.noreply.github.com>
2024-06-29 15:39:19 +0100

    h

--- models/place.py
+++ models/place.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
-""" Place Module for HBNB project """
+"""Place Module for HBNB project"""
+
 from models.base_model import BaseModel, Base
 from sqlalchemy import String, ForeignKey, Integer, Float, Column
 from sqlalchemy.orm import relationship
 
 
 class Place(BaseModel, Base):
-    """ A place to stay """
-    __tablename__ = 'places'
-    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
-    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    """A place to stay"""
+
+    __tablename__ = "places"
+    city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
+    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
     name = Column(String(128), nullable=False)
     description = Column(String(1024))
     number_rooms = Column(Integer, default=0, nullable=False)
@@ -20,16 +22,16 @@ class Place(BaseModel, Base):
     longitude = Column(Float)
     amenity_ids = []
 
-    reviews = relationship(
-                    'Review', backref='place', cascade='all, delete-orphan')
+    reviews = relationship("Review", backref="place", cascade="all, delete-orphan")
 
     @property
-    def reviews(delf):
-        """ getter attribute that retrun the list of review"""
+    def reviews(self):
+        """getter attribute that retrun the list of review"""
         from models import storage
         from models.review import Review
+
         list_rev = []
         for rev in storage.all(Review).values():
             if Review.place_id == self.id:
-                list_rew.append(rev)
+                list_rev.append(rev)
         return list_rev
